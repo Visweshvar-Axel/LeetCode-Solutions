d49c9cdd9566d13d849278b302007c69f3cc6ff3
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-08 23:07:47 +0530

    20. Valid Parentheses

---
+++ 20_ValidParentheses/ValidParentheses.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+class Solution {
+    boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                char temp = stack.pop();
+                if (c == '}' && temp != '{' || c == ']' && temp != '[' || c == ')' && temp != '(')
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    /*
+     * *
+     * bool isValid(string s) {
+     * char[] stack = new char[s.length()];
+     * int t=-1;
+     * for(char c: s.toCharArray()){
+     * if (c == '{' || c == '[' || c == '(') {
+     * stack[++t] = c;
+     * } else {
+     * char temp = stack[t--];
+     * if (c == '}' && temp != '{' || c == '}' && temp != '{' || c == '}' && temp !=
+     * '{') {
+     * return false;
+     * }
+     * }
+     * }
+     * return t == -1;
+     * }
+     * }
+     */
+}
